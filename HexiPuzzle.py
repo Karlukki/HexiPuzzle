4b7344091cfee46b2880de25b7559e5cfc96e12c
Yettiie <etamisars@gmail.com>
2023-10-24 11:29:59 +0300

    Added landing screen with start button and loading progress bar

--- HexiPuzzle.py
+++ HexiPuzzle.py
@@ -67,48 +67,81 @@ def point_in_triangle(pt, triangle):
 
     return b1 == b2 == b3
 
+loading_progress = 0
+def draw_loading_line():
+    pygame.draw.line(screen, 'black', (0, 0), (loading_progress, 0), 5)
+
+
+def draw_start_screen():
+    screen.fill('burlywood3')
+
+    start_button_rect = pygame.Rect(400, 300, 200, 50)
+    pygame.draw.rect(screen, 'orange', start_button_rect)
+    font = pygame.font.Font(None, 36)
+    text = font.render("START", True, 'black')
+    text_rect = text.get_rect(center=start_button_rect.center)
+    screen.blit(text, text_rect)
+
 active_hex = None
-hex_colors = ['deeppink', 'darkgoldenrod1', 'purple', 'cyan2', 'darkgreen', 'yellow', 'cornflowerblue', 'darkorange1', 'lime', 'bisque', 'darkolivegreen4', 'indianred1']
+hex_colors = ['deeppink', 'darkgoldenrod1', 'purple', 'cyan2', 'darkgreen', 'yellow', 'cornflowerblue',
+              'darkorange1', 'lime', 'bisque', 'darkolivegreen4', 'indianred1']
 hex_colors_index = 0
 
-run = True
-while run:
+def draw_game_screen():
+    global hex_colors_index  # Declare hex_colors_index as a global variable
     screen.fill('burlywood3')
     for hex in hexiamonds:
         for tri in calculateCoords(hex):
-            if hex_colors_index > 11:
-                hex_colors_index = 0
-
             pygame.draw.polygon(screen, hex_colors[hex_colors_index], tri)
             pygame.draw.polygon(screen, 'black', tri, 3)
-        hex_colors_index += 1
+        hex_colors_index = (hex_colors_index + 1) % len(hex_colors)
 
 
+
+
+current_screen = 'start'
+
+
+run = True
+while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
-                for num, hex in enumerate(hexiamonds):
-                    for tri in calculateCoords(hex):
-                        if point_in_triangle(event.pos, tri):
-                            active_hex = num
-                            break
+                if current_screen == 'start':
+                    start_button_rect = pygame.Rect(400, 300, 200, 50)
+                    if start_button_rect.collidepoint(event.pos):
+                        current_screen = 'loading'
+                elif current_screen == 'game':
+                    for num, hex in enumerate(hexiamonds):
+                        for tri in calculateCoords(hex):
+                            if point_in_triangle(event.pos, tri):
+                                active_hex = num
+                                break
 
         if event.type == pygame.MOUSEBUTTONUP:
             if event.button == 1:
                 active_hex = None
 
         if event.type == pygame.MOUSEMOTION:
-            if active_hex != None:
+            if current_screen == 'game' and active_hex is not None:
                 dx, dy = event.rel
                 current_origin = hexiamonds[active_hex]['origin']
-
                 new_x = max(0, min(current_origin[0] + dx, SCREEN_WIDTH))
                 new_y = max(0, min(current_origin[1] + dy, SCREEN_HEIGHT))
                 hexiamonds[active_hex]['origin'] = (new_x, new_y)
 
+    if current_screen == 'start':
+        draw_start_screen()
+    elif current_screen == 'loading':
+        draw_loading_line()
+        loading_progress += 2
+        if loading_progress >= SCREEN_WIDTH:
+            current_screen = 'game'
+    elif current_screen == 'game':
+        draw_game_screen()
 
     pygame.display.flip()
 
